1828a875b25dec72b6d7e8e0cb520c9d18d6be0d
rohansen856 <rohansen856@gmail.com>
2025-04-11 22:59:44 +0530

    basic todo app created

---
+++ app/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { Pool } = require('pg');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+app.get('/todos', async (_, res) => {
+    const result = await pool.query('SELECT * FROM todos');
+    res.json(result.rows);
+});
+
+app.post('/todos', async (req, res) => {
+    const { title } = req.body;
+    const result = await pool.query('INSERT INTO todos(title) VALUES($1) RETURNING *', [title]);
+    res.json(result.rows[0]);
+});
+
+app.listen(port, () => {
+    console.log(`Todo app running on port ${port}`);
+});
